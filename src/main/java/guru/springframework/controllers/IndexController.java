11c6c46dff8fcf8348bcf75d7d6df4220543e9b3
msulbara <msulbara@MSULBARA-AR.ar.oracle.com>
2020-11-02 20:09:44 -0300

    refactor

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,16 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package io.springframework.controllers;
+package guru.springframework.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- *
- * @author msulbara
+ * Created by jt on 6/1/17.
  */
 @Controller
 public class IndexController {
