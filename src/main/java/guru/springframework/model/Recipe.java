e08b1b746d93e0cd8b840ce2eb2e1eecfea6ef63
msulbara <msulbara@MSULBARA-AR.ar.oracle.com>
2020-11-17 20:38:23 -0300

    Defficulty enum added to model

--- src/main/java/guru/springframework/model/Recipe.java
+++ src/main/java/guru/springframework/model/Recipe.java
@@ -8,6 +8,8 @@ package guru.springframework.model;
 import java.util.Set;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -41,6 +43,9 @@ public class Recipe {
 
     @Lob
     private Byte[] image;
+    
+    @Enumerated (value = EnumType.STRING)
+    private Difficulty difficulty;
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
@@ -123,6 +128,14 @@ public class Recipe {
 
     public void setImage(Byte[] image) {
         this.image = image;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 
     public Notes getNotes() {
